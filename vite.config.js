d32c596aa37acbccfb3e52ea72fb542f0f2c751c
hosain ali <hosainahmed534745@gmail.com>
2025-04-28 14:22:42 +0600

    update favicons and name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
-    host: '209.97.134.184',
+    // host: '209.97.134.184',
+    host: '0.0.0.0',
     port: 4174,
   },
   preview: {
